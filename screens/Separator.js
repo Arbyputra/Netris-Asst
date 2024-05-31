bb3d45ee8b6780f3c6cd1a91fb89eeea7f3ffe2c
unknown <m.toifur.21@student.is.ittelkom-sby.ac.id>
2024-05-31 09:31:17 +0700

    push rating dan modal rating

---
+++ screens/Separator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Separator = ({ h }) => {
+  return <View style={{ height: h }} />;
+};
+
+export default Separator;
---
+++ screens/nerby.js
@@ -0,0 +1,267 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Dimensions,
+  Image,
+  Button,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import { Rating } from "react-native-ratings";
+import Modal from 'react-native-modal';
+import Separator from './Separator'; // Pastikan jalur relatif sesuai
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  ratingBackground: {
+    backgroundColor: "#DCCDE5",
+    padding: 10,
+    borderRadius: 10,
+  },
+  modalContainer: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalContent: {
+    width: '100%',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
+
+const Nerby = ({ navigation }) => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [chooseItem, setChooseItem] = useState(0);
+  const [ratings, setRatings] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [temporaryRating, setTemporaryRating] = useState(0);
+
+  useEffect(() => {
+    const fetchCurrentLocation = async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          setError("Permission to access location was denied");
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        setCurrentLocation(location.coords);
+      } catch (error) {
+        setError("Error getting user location: " + error);
+      }
+    };
+    fetchCurrentLocation();
+  }, []);
+
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
+
+  const listTambalBan = [
+    {
+      id: 0,
+      nama: "Tambal ban cak imin",
+      tipe: "Bengkel motor",
+      alamat: "Jl bareng cuma temen",
+    },
+    {
+      id: 1,
+      nama: "Tambal ban jetis kulon",
+      tipe: "Bengkel motor",
+      alamat: "Jl bareng cuma temen",
+    },
+    {
+      id: 2,
+      nama: "Tambal ban mas bro",
+      tipe: "Bengkel motor",
+      alamat: "Jl bareng cuma temen",
+    },
+    {
+      id: 3,
+      nama: "Tambal ban sis",
+      tipe: "Bengkel mobil",
+      alamat: "Jl bareng cuma temen",
+    },
+    {
+      id: 4,
+      nama: "Tambal ban pak dono",
+      tipe: "Bengkel mobil",
+      alamat: "Jl bareng cuma temen",
+    },
+    {
+      id: 5,
+      nama: "Tambal ban banjaya",
+      tipe: "Bengkel motor",
+      alamat: "Jl bareng cuma temen",
+    },
+    {
+      id: 6,
+      nama: "Tambal ban barokah",
+      tipe: "Bengkel motor",
+      alamat: "Jl bareng cuma temen",
+    },
+  ];
+
+  const handleRatingCompleted = (rating) => {
+    setTemporaryRating(rating);
+  };
+
+  const saveRating = () => {
+    if (selectedItem) {
+      setRatings((prevRatings) => ({
+        ...prevRatings,
+        [selectedItem.id]: temporaryRating,
+      }));
+      setModalVisible(false);
+    }
+  };
+
+  const openRatingModal = (item) => {
+    setSelectedItem(item);
+    setTemporaryRating(ratings[item.id] || 0);
+    setModalVisible(true);
+  };
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          setChooseItem(item.id);
+          openRatingModal(item);
+        }}
+        style={{
+          height: windowHeight * 0.22,
+          width: windowWidth * 0.8,
+          borderRadius: 10,
+          backgroundColor: item.id === chooseItem ? "#DCCDE5" : "#FFFFFF",
+          borderWidth: 2,
+          borderColor: "#A7A7A7",
+          marginHorizontal: 15,
+          marginVertical: 20,
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Image
+            style={{
+              width: "100%",
+              height: "100%",
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+            }}
+            source={require("../assets/tambalBan.jpg")}
+          />
+        </View>
+        <View style={{ flex: 1.3, paddingLeft: 10, justifyContent: "center" }}>
+          <Text
+            style={{
+              fontFamily: "Inter_700Bold",
+              fontSize: 16,
+              color: "#5A1781",
+            }}
+          >
+            {item.nama}
+          </Text>
+          <View style={{ height: 3 }} />
+          <Text
+            style={{
+              fontFamily: "Inter_400Regular",
+              fontSize: 12,
+            }}
+          >
+            {item.tipe}
+          </Text>
+          <View style={{ height: 3 }} />
+          <Text
+            style={{
+              fontFamily: "Inter_400Regular",
+              fontSize: 12,
+            }}
+          >
+            {item.alamat}
+          </Text>
+          <View style={{ height: 5 }} />
+          <View style={styles.ratingBackground}>
+            <Rating
+              type="star"
+              ratingCount={5}
+              imageSize={20}
+              showRating={false}
+              readonly
+              startingValue={ratings[item.id] || 0}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 3 }}>
+        <MapView
+          showsUserLocation={true}
+          showsCompass={true}
+          initialRegion={{
+            latitude: parseFloat(-7.3385169),
+            longitude: parseFloat(112.719163),
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </View>
+      <View
+        style={{ flex: 1.5, justifyContent: "center", alignItems: "center" }}
+      >
+        <FlatList
+          data={listTambalBan}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+      <Modal
+        isVisible={modalVisible}
+        onBackdropPress={() => setModalVisible(false)}
+        onSwipeComplete={() => setModalVisible(false)}
+        swipeDirection="down"
+        style={styles.modalContainer}
+      >
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Rate {selectedItem?.nama}</Text>
+          <View style={styles.ratingBackground}>
+            <Rating
+              type="star"
+              ratingCount={5}
+              imageSize={40}
+              showRating
+              onFinishRating={handleRatingCompleted}
+              startingValue={temporaryRating}
+            />
+          </View>
+          <Button title="Save" onPress={saveRating} />
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default Nerby;
